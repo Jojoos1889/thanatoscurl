b2147d8f10184a48f9a5c45bdb195e47f09a0ec7
Jojoos1889 <113982420+Jojoos1889@users.noreply.github.com>
2023-05-18 14:43:52 +0200

    Update thanatos.sh

--- thanatos.sh
+++ thanatos.sh
@@ -5,7 +5,8 @@ echo ╰╮╭┫╰━┳━━┳━╮╭━┻╮╭╋━━┳━━╮
 echo ╱┃┃┃╭╮┃╭╮┃╭╮┫╭╮┃┃┃╭╮┃━━┫
 echo ╱┃╰┫┃┃┃╭╮┃┃┃┃╭╮┃╰┫╰╯┣━━┃
 echo ╱╰━┻╯╰┻╯╰┻╯╰┻╯╰┻━┻━━┻━━╯
-sudo apt install nmap testssl.sh nikto
+read -p "target:" targetvar
+
 nmap -Pn -T4 -A -p- $targetvar
 testssl -9 $targetvar
 nikto --host $targetvar
